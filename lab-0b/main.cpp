41e92dacbe8d41a8a9bfe908472c8a3bc4f2abab
victor-lozhnikov <46621169+victor-lozhnikov@users.noreply.github.com>
2019-09-04 21:15:31 +0700

    Update main.cpp

--- lab-0b/main.cpp
+++ lab-0b/main.cpp
@@ -2,38 +2,70 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-struct cmpByFreq {
-	bool operator()(const string& a, const pair<string, int>& b) const {
-		return b.second < b.first;
-	}
-};
+bool is_correct_char (unsigned char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+}
+
+bool comp (pair<string, double> a, pair<string, double> b) {
+    return a.second < b.second;
+}
 
 int main(int argv, char** argc) {
-	ifstream input("input.txt");
-	ofstream output("output.csv");
+    ifstream input("input.txt");
+    ofstream output("output.csv");
 
-	map <string, pair<int, double>> words;
-	int word_count = 0;
+    map <string, int> word_count;
+    int all_words_count = 0;
 
-	string str;
-	while (!input.eof()) {
-		getline(input, str, ' ');
-		word_count++;
-		if (words.find(str) == words.end()) {
-			words[str].first = 1;
-		}
-		else {
-			words[str].first++;
-		}
-		words[str].second = 1.0 / word_count;
-	}
+    string str;
+    while (!input.eof()) {
+        getline(input, str, ' ');
+        vector <string> words_in_line;
+        int vec_index = 0;
+        bool in_word = false;
+        for (int i = 0; i < str.size(); ++i) {
+            if (is_correct_char(str[i])) {
+                if (in_word) {
+                    words_in_line[vec_index].push_back(str[i]);
+                }
+                else {
+                    words_in_line.emplace_back("");
+                    words_in_line[vec_index].push_back(str[i]);
+                    in_word = true;
+                    all_words_count++;
+                }
+            }
+            else {
+                if (in_word) {
+                    in_word = false;
+                    vec_index++;
+                }
+            }
+        }
+        for (auto i : words_in_line) {
+            if (word_count.find(i) == word_count.end()) {
+                word_count[i] = 1;
+            }
+            else {
+                word_count[i]++;
+            }
+        }
+    }
 
+    vector <pair <string, double>> frequencies;
 
+    for (auto i : word_count) {
+        frequencies.emplace_back(make_pair(i.first, (double) i.second / all_words_count));
+    }
+    
+    sort (frequencies.rbegin(), frequencies.rend(), comp);
 
-	for (auto i : words) {
-		cout << i.first << " " << i.second.first << " " << i.second.second << "\n";
-	}
+    for (auto i : frequencies) {
+        output << i.first << "," << word_count[i.first] << "," << i.second << "\n";
+    }
 }
